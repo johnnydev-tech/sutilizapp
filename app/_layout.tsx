7c7ed03530d2f77f64036fc0f5cdb61c4153c373
johnnydev-tech <johnnyfreirejh@hotmail.com>
2024-12-15 12:05:42 -0300

    refcator :aaded header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{
+    headerShown: true,
+    headerTitle: "Ofenda com classe", 
+    headerStyle: {
+      backgroundColor: '#6200ee', 
+    },
+    headerTintColor: '#ffffff', 
+  }}
+  />;
 }
